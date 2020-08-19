309bd9a3fd48bd51b4dea79d5ef2c65728d79286
Tomi-RCode <tomy1goro@gmail.com>
2020-08-19 15:55:47 -0300

    agregado de imagenes

--- index.php
+++ index.php
@@ -2,62 +2,98 @@
 <!doctype html>
 <html lang="en">
 
+<!-- https://www.el-mueble.com.ar/showroom/ -->
+
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-  <title>Hello, world!</title>
+  <title>Koro Muebles</title>
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Koro Muebles</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Productos
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Cama</a>
+          <a class="dropdown-item" href="#">Sillon</a>
+          <a class="dropdown-item" href="#">Biblioteca</a>
+          <a class="dropdown-item" href="#">Comoda</a>
+          <a class="dropdown-item" href="#">Barra</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">otro</a>
+        </div>
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          <?php
-          for ($i = 0; $i < 5; $i++) {
-            echo '<li class="nav-item">';
-            echo "<a class='nav-link' href='#'>Link $i</a>";
-            echo '</li>';
-          }
-          ?>
+          <a class="nav-link" href="#">Promociones <span class="sr-only">(current)</span></a>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Novedades <span class="sr-only">(current)</span></a>
+        <li class="nav-item dropdown">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Registrarse/iniciar sesion <span class="sr-only">(current)</span></a>
+        <li class="nav-item dropdown">
+        <li class=" nav-item active">
+          <a class="nav-link" href="#">Contacto <span class="sr-only">(current)</span></a>
+        <li class="nav-item dropdown">
         </li>
       </ul>
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Buscar</button>
       </form>
     </div>
   </nav>
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  <!-- PORTADA -->
+  <!--   <div class="card bg-dark text-white">
+    <img src="Imagenes\Portada.jpg" class="card-img" alt="Responsive image">
+    <div class="card-img-overlay">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text">Last updated 3 mins ago</p>
+    </div>
+  </div> -->
+
+  <!-- PRODUCTOS -->
+  <div class="row">
+    <div class="col-sm-4">
+      <img src="Imagenes\Cama.jfif" class="img-thumbnail" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Cama</h5>
+        <p class="card-text">Breve descripcion</p>
+        <a href="#" class="btn btn-primary">Ver mas</a>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <img src="Imagenes\biblioteca.jpg" class="img-thumbnail" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Cama</h5>
+        <p class="card-text">Breve descripcion</p>
+        <a href="#" class="btn btn-primary">Ver mas</a>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <img src="Imagenes\Comoda.jpg" class="img-thumbnail" alt="">
+      <div class="card-body">
+        <h5 class="card-title">Cama</h5>
+        <p class="card-text">Breve descripcion</p>
+        <a href="#" class="btn btn-primary">Ver mas</a>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
